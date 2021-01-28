b86f3ea2520a4390521bc097c9a2e01a39374aed
a.vasilev <a.vasilev@qsoft.ru>
2021-01-28 21:39:18 +0300

    Add Job

---
+++ app/Jobs/SendMessageJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class SendMessageJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     *
+     * @param array $params
+     * @return void
+     * @throws \Exception
+     */
+    public function handle(array $params)
+    {
+        $response = Http::post('https://send.message/api/send', $params);
+
+        if ($response->status()) {
+            throw new \Exception('Не удалось отправить сообщение');
+        }
+    }
+}
